691cd861e37e651d8609f0956cac9d7949ceaf74
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-03-22 14:29:30 +0530

    Create after signIn Design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,26 @@
 "use client";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 
 export default function Home() {
 
-  const {data: session} = useSession;
+  const {data: session} = useSession();
 
   if(session)
   {
     return(
-      <div>
-        <h1>Your Profile</h1>
+      <div className="flex h-screen items-center justify-center">
+
+      <div className="bg-white p-8 rounded-lg shadow-md">
+
+        <p className="text-xl font-semibold mb-4">Welcome {session.user?.name}. Signed In As</p>
+        <p>{session.user?.email}</p>
+        <img src={session.user?.image} alt="" />
+
+        <button onClick={() => signOut()} className="mt-4 bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md">Logout</button>
+
+      </div>
+        
       </div>
       );
   }
